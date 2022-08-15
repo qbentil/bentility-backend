0648b1e1d27a9f62d0ba5abff8fe038c2d7361ef
qbentil <bentilshadrack72@gmail.com>
2022-08-15 19:06:54 +0000

    Create Error Util

---
+++ utils/Error.js
@@ -0,0 +1,7 @@
+//  create error function
+export const createError = (message, statusCode) => {
+    const error = new Error();
+    error.message = message;
+    error.statusCode = statusCode;
+    return error;
+}
