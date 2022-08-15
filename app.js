618d4dfb2c327fd1d1069c398c8d9cdf0f01a0c8
qbentil <bentilshadrack72@gmail.com>
2022-08-15 19:07:25 +0000

    Import Home Route

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-import DBCONNECT from './database/index.js';
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import express from 'express'
-import mongoose from 'mongoose'
+import DBCONNECT from "./database/index.js";
+import { HomeRoute } from "./routes/index.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
 // init dotenv
-dotenv.config()
+dotenv.config();
 
 // init app
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// middleware
-
+// HOME ROUTE
+app.use("/", HomeRoute);
 
 // run server
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    DBCONNECT();
-  console.log(`Server running on port ${port} 🚀`)
-})
+  DBCONNECT();
+  console.log(`Server running on port ${port} 🚀`);
+});
