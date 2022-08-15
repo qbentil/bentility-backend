624317d18c7e22d5ac040f6c6c151f40a586c856
qbentil <bentilshadrack72@gmail.com>
2022-08-15 19:07:10 +0000

    Create Error Handling middleware

---
+++ middlewares/ErrorHandler.js
@@ -0,0 +1,12 @@
+// Create error handling middleware
+const errorHandler = (err, req, res, next) => {
+    console.log("Middleware Error Hadnling");
+    const errStatus = err.statusCode || 500;
+    const errMsg = err.message || 'Something went wrong';
+    res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+        stack: err.stack
+    })
+}
