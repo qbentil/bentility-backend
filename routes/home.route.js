bcbe6d0d18ea9e65426d63c87b47298cd33f9088
qbentil <bentilshadrack72@gmail.com>
2022-08-16 00:08:26 +0000

    Update homepage

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,45 +1,10 @@
+import { HomeController } from '../controllers/home.controller.js';
 import express from 'express';
 
 // init express router
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.json({
-      message: "Welcome to the Bentility Blog API",
-      repository_url: "https://github.com/qbentil.com/bentility-api",
-      base_url: "http://localhost:3000",
-      endpoints: [
-        {
-          method: "GET",
-          path: "/api/v1/posts",
-          description: "Get all posts",
-        },
-        {
-          method: "POST",
-          path: "/api/v1/posts",
-          description: "Create a new post",
-        },
-        {
-          method: "GET",
-          path: "/api/v1/posts/:postId",
-          description: "Get a post by id",
-        },
-        {
-          method: "PUT",
-          path: "/api/v1/posts/:postId",
-          description: "Update a post by id",
-        },
-        {
-          method: "DELETE",
-          path: "/api/v1/posts/:postId",
-          description: "Delete a post by id",
-        },
-      ],
-      author: "Shadrack Bentil",
-      author_url: "https://qbentil.com",
-      
-    });
-  });
+router.get('/', HomeController);
   
 // export router
 export default router;
