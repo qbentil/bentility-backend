5c9e06f84205135ff491df5106a10d4041e3294c
qbentil <bentilshadrack72@gmail.com>
2022-08-16 00:07:07 +0000

    Create user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: ['writer', 'admin'],
+        default: 'writer'
+    },
+    about: {
+        type: String,
+    },
+    phone: {
+        type: String
+    },
+    avatar: {
+        type: String
+    }
+}, { timestamps: true });
+
+export default mongoose.models['User'] || mongoose.model('User', UserSchema);
