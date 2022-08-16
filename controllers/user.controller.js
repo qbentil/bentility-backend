0828320072c9f36b96d4f39724a018e252198b20
qbentil <bentilshadrack72@gmail.com>
2022-08-16 15:49:43 +0000

    Do magic with user controller✨🚀

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,8 +3,7 @@ import bcrypt from "bcryptjs";
 
 // ADD USER
 export const addUser = async (req, res, next) => {
-  const { username, email, password, name, role, about, phone, avatar } =
-    req.body;
+  const { username, email, password, name, role, about, phone, avatar } = req.body;
   let salt = bcrypt.genSaltSync(10);
   let hash = bcrypt.hashSync(password, salt);
   try {
@@ -45,9 +44,7 @@ export const updateUser = async (req, res, next) => {
         about,
         phone,
         avatar,
-      },
-      { new: true }
-    );
+      },{ new: true });
     // remove password and token
     const { password, token, ...userData } = user._doc;
     res.status(200).json({
@@ -65,12 +62,18 @@ export const deleteUser = async (req, res, next) => {
   const { id } = req.params;
   try {
     const user = await User.findByIdAndDelete(id);
+    if(!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
     // remove password and token
     const {password, token, ...userData} = user._doc;
     res.status(200).json({
-      success: true,
-      message: "User deleted successfully",
-      user: userData,
+        success: true,
+        message: "User deleted successfully",
+        user: userData,
     });
   } catch (error) {
     next(error);
@@ -79,56 +82,71 @@ export const deleteUser = async (req, res, next) => {
 
 // GET USERS
 export const getUsers = async (req, res, next) => {
+  const {key, value} = req.body;
+  const keys = ["name", "role", "about"];
+  if(key && !keys.includes(key)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid key",
+    });
+  }
   try {
-    const users = await User.find();
+    const users = key? await User.find({[key]: value}): await User.find();
+    if(users.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Users not found",
+      });
+    }
     // filter out password, is_deleted and is_super from admin data
     const usersData = users.map((user) => {
         const { password, token, ...userData } = user._doc;
         return userData;
     })
     res.status(200).json({
-      success: true,
-      message: "Users retrieved successfully",
-      users: usersData,
+        success: true,
+        message: "Users retrieved successfully",
+        total: users.length,
+        users: usersData,
     });
   } catch (error) {
     next(error);
   }
 };
 
-// GET USER BY ID
-export const getUserById = async (req, res, next) => {
-  const { id } = req.params;
+// GET SINGLE USER 
+export const getUser = async (req, res, next) => {
+    const {key, value} = req.body;
+    console.log(req.body);
+    const keys = ["username", "email", "phone"];
+    if(!keys.includes(key)) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid query key <[username, email, phone]>",
+        });
+    }
   try {
-    const user = await User.findById(id);
+    const user = await User.findOne({[key]: value});
+    if(!user) {
+        res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
     // remove password and token
-    const {password, token, ...userData} = user._doc;
+    const { password, token, ...userData } = user._doc;
     res.status(200).json({
-      success: true,
-      message: "User retrieved successfully",
-      user: userData,
+        success: true,
+        message: "User retrieved successfully",
+        user: userData,
     });
+
+    
   } catch (error) {
     next(error);
   }
 };
 
-// GET USER BY EMAIL
-export const getUserByEmail = async (req, res, next) => {
-  const { email } = req.params;
-  try {
-    const user = await User.findOne({ email });
-    // remove password and token
-    const {password, token, ...userData} = user._doc;
-    res.status(200).json({
-      success: true,
-      message: "User retrieved successfully",
-      user: userData,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
 // CHANGE PASSWORD
 export const changePassword = async (req, res, next) => {
@@ -137,19 +155,49 @@ export const changePassword = async (req, res, next) => {
     let salt = bcrypt.genSaltSync(10);
     let hash = bcrypt.hashSync(password, salt);
     try {
-        const user = await User.findByIdAndUpdate(
-        id,
-        {
-            password: hash,
-        },
-        { new: true }
-        );
+        const user = await User.findByIdAndUpdate(id,{ password: hash, }, { new: true });
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
         // remove password and token
         const { password, token, ...userData } = user._doc;
         res.status(200).json({
         success: true,
         message: "Password changed successfully",
         user: userData,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+// USER AUTHENTICATION
+export const userAuth = async (req, res, next) => {
+    const { email, secret } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+        const isMatch = bcrypt.compareSync(secret, user.password);
+        if(!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials",
+            });
+        }
+        // remove password and token
+        const { password, token, ...userData } = user._doc;
+        res.status(200).json({
+            success: true,
+            message: "User authenticated successfully",
+            user: userData,
         });
     } catch (error) {
         next(error);
