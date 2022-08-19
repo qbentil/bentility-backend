f9e485c6535cb9f537e72cc0798f222b02fdb95b
qbentil <bentilshadrack72@gmail.com>
2022-08-19 10:13:35 +0000

    Create Admin controllers

---
+++ controllers/admin.controllers.js
@@ -0,0 +1,175 @@
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+
+// ADD USER
+export const addUser = async (req, res, next) => {
+  const { username, email, password, name, role, about, phone, avatar } = req.body;
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(password, salt);
+  try {
+    const user = new User({
+      username,
+      email,
+      password: hash,
+      name,
+      role,
+      about,
+      phone,
+      avatar,
+    });
+    await user.save();
+
+    // remove password and token
+    const { password, token, ...userData } = user._doc;
+    res.status(201).json({
+      success: true,
+      message: "User added successfully",
+      user: userData,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// UPDATE USER
+export const updateUser = async (req, res, next) => {
+  const { id } = req.user;
+  const { name, role, about, phone, avatar } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        role,
+        about,
+        phone,
+        avatar,
+      },{ new: true });
+    // remove password and token
+    const { password, token, ...userData } = user._doc;
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      user: userData,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// DELETE USER
+export const deleteUser = async (req, res, next) => {
+  const { id } = req.user;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if(!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    // remove password and token
+    const {password, token, ...userData} = user._doc;
+    res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+        user: userData,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// GET USERS
+export const getUsers = async (req, res, next) => {
+  const {key, value} = req.body;
+  const keys = ["name", "role", "about"];
+  if(key && !keys.includes(key)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid key",
+    });
+  }
+  try {
+    const users = key? await User.find({[key]: value}): await User.find();
+    if(users.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Users not found",
+      });
+    }
+    // filter out password, is_deleted and is_super from admin data
+    const usersData = users.map((user) => {
+        const { password, token, ...userData } = user._doc;
+        return userData;
+    })
+    res.status(200).json({
+        success: true,
+        message: "Users retrieved successfully",
+        total: users.length,
+        users: usersData,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// GET SINGLE USER 
+export const getUser = async (req, res, next) => {
+    const {key, value} = req.body;
+    console.log(req.body);
+    const keys = ["username", "email", "phone"];
+    if(!keys.includes(key)) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid query key <[username, email, phone]>",
+        });
+    }
+  try {
+    const user = await User.findOne({[key]: value});
+    if(!user) {
+        res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
+    // remove password and token
+    const { password, token, ...userData } = user._doc;
+    res.status(200).json({
+        success: true,
+        message: "User retrieved successfully",
+        user: userData,
+    });
+
+    
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+// CHANGE PASSWORD
+export const changePassword = async (req, res, next) => {
+    const { id } = req.user;
+    const { password } = req.body;
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+    try {
+        const user = await User.findByIdAndUpdate(id,{ password: hash, }, { new: true });
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+        // remove password and token
+        const { password, token, ...userData } = user._doc;
+        res.status(200).json({
+        success: true,
+        message: "Password changed successfully",
+        user: userData,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
