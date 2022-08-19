3abae880d32f6b6095a19311f008c3d75cabc5f3
qbentil <bentilshadrack72@gmail.com>
2022-08-19 10:13:05 +0000

    Create Admin Route

---
+++ routes/admin.routes.js
@@ -0,0 +1,26 @@
+import { addUser, changePassword, deleteUser, getUser, getUsers, updateUser } from "../controllers/admin.controllers.js";
+
+import { Router } from "express";
+
+const router = Router();
+// ADD USER
+router.post("/", addUser)
+
+// UPDATE USER
+router.put("/:id", updateUser)
+
+// DELETE USER
+router.delete("/:id", deleteUser)
+
+// GET USERS
+router.get("/", getUsers)
+
+// GET USER
+router.get("/user", getUser)
+
+
+// CHANGE PASSWORD
+router.patch("/password/:id", changePassword)
+
+
+export default router;
