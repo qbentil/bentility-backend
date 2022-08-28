ae67927417e2de8b968a44778b2939e4dfe9ecb8
qbentil <bentilshadrack72@gmail.com>
2022-08-28 00:26:53 +0000

    Add RefreshToken route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { AUTHENTICATION, LOGOUT } from "../controllers/auth.controllers.js";
+import { AUTHENTICATION, GETREFRESHTOKEN, LOGOUT } from "../controllers/auth.controllers.js";
 
 const router = Router()
 
 router.post("/", AUTHENTICATION)
 router.get("/", LOGOUT)
+router.get("/refresh", GETREFRESHTOKEN)
 
 export default router;
