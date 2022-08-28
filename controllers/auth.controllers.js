60de6e3de75081d7a8c096917ec2e996e4b7dc6a
qbentil <bentilshadrack72@gmail.com>
2022-08-28 00:27:28 +0000

    Create refresh token controller

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,8 @@
 import GenerateToken from "../utils/Tokens.js";
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import CreateError from "../utils/Error.js";
 
 // USER AUTHENTICATION
 export const AUTHENTICATION = async (req, res, next) => {
@@ -79,3 +81,40 @@ export const LOGOUT = async (req, res, next) => {
         next(error);
     }
 }
+export const GETREFRESHTOKEN = async (req, res, next) => {
+    try {
+
+        const cookies = req.cookies
+        if(!cookies?.refresh_token) return next(CreateError("Unauthorized", 401)) // unauthorized
+        const refresh_token = cookies.refresh_token
+        console.log("token >>>>", refresh_token)
+        const user = await User.findOne({token:refresh_token})
+
+        if(!user){
+            console.log("No user found")
+            return next(CreateError("Forbidden Access", 403))
+        }
+
+         const decoded = jwt.verify(refresh_token, process.env.JWT_REFRESH_SECRET || "")
+
+         console.log("decoded >>>", decoded)
+            // now generate a new access token
+            const access_token = jwt.sign({id: decoded.id, role:decoded.role  }, process.env.JWT_SECRET || "", {expiresIn: "1d"}) 
+            
+            // remove refresh_token, role, password from user object
+                const {password, token, role, ...userData} = user._doc;
+            // append access token to user
+            userData.access_token = access_token
+            res.status(201).json({
+                success: true,
+                user:userData,
+                message: "new token generated"
+            })  
+            
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
