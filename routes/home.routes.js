a69ac46fd78283602cee8dde41f383079d91a575
qbentil <bentilshadrack72@gmail.com>
2022-09-29 14:53:10 +0000

    Test email on home route

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,10 +1,14 @@
-import { HomeController } from '../controllers/home.controllers.js';
+import { HomeController, TestEmail } from '../controllers/home.controllers.js';
+
 import express from 'express';
 
 // init express router
 const router = express.Router();
 
 router.get('/', HomeController);
+
+// SEND EMAIL
+router.post("/email", TestEmail)
   
 // export router
 export default router;
