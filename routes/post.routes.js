51faac3e651f7b0183fcd95bec60bd142f70feaa
qbentil <bentilshadrack72@gmail.com>
2022-10-02 02:13:23 +0000

    Update post

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,4 +1,4 @@
-import { createPost, getPost, getPosts } from "../controllers/post.controllers.js";
+import { createPost, getPost, getPosts, updatePost } from "../controllers/post.controllers.js";
 
 import { Router } from "express";
 import { verifyAccessToken } from "../middlewares/Verifications.js";
@@ -9,6 +9,9 @@ router.post("/", verifyAccessToken, createPost);
 
 // READ POSTS
 router.get("/", getPosts); 
+
+// UPDATE POST
+router.put("/:id", verifyAccessToken, updatePost);
 // READ POST BY ID
 router.get("/post", getPost)
 
