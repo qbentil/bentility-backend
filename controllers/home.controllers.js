e11ce3effea03b05edb1e827c5ba958f97a070fc
qbentil <bentilshadrack72@gmail.com>
2022-10-16 02:33:43 +0000

    Send public mail from home controller

--- controllers/home.controllers.js
+++ controllers/home.controllers.js
@@ -2,42 +2,28 @@ import Mail from "../mail/index.js";
 import User from "../models/user.js";
 
 export const HomeController = (req, res, next) => {
-    res.json({
-      message: "Welcome to the Bentility Blog API",
-      repository_url: "https://github.com/qbentil.com/bentility-api",
-      base_url: "http://localhost:3000",
-      endpoints: [
-        {
-          method: "GET",
-          path: "/posts",
-          description: "Get all posts",
+  res.json({
+    message: "Welcome to the Bentility Blog API",
+    repository_url: "Sorry this is a private repository",
+    base_url: "https://bentility-api.herokuapp.com",
+    public_endpoints: [
+      {
+        method: "POST",
+        path: "/public/sendmail",
+        description: "Send email to anyone with our public mailer",
+        body: {
+          receiver_email: "string" || "required",
+          subject: "String" || "required",
+          message: "String" || "required",
+          sender_name: "String" || "optional",
+          sender_email: "String" || "optional",
         },
-        {
-          method: "POST",
-          path: "/posts",
-          description: "Create a new post",
-        },
-        {
-          method: "GET",
-          path: "/posts/:postId",
-          description: "Get a post by id",
-        },
-        {
-          method: "PUT",
-          path: "/posts/:postId",
-          description: "Update a post by id",
-        },
-        {
-          method: "DELETE",
-          path: "/posts/:postId",
-          description: "Delete a post by id",
-        },
-      ],
-      author: "Shadrack Bentil",
-      author_url: "https://qbentil.com",
-      
-    });
-}
+      },
+    ],
+    author: "Shadrack Bentil",
+    author_url: "https://qbentil.com",
+  });
+};
 
 export const SENDMAIL = async (req, res, next) => {
   try {
@@ -45,19 +31,72 @@ export const SENDMAIL = async (req, res, next) => {
       email: req.body.email,
       subject: req.body.subject,
       message: req.body.message,
-
-    }
-    Mail.SendMail(data, (info)=> {
+    };
+    Mail.SendMail(data, (info) => {
       res.status(200).json({
         success: true,
         message: "Email sent successfully",
-        info
-      })
-    })
-  }catch (error) {
+        info,
+      });
+    });
+  } catch (error) {
     next(error);
   }
-}
+};
+
+// PUBLIC SEND MAIL
+export const PUBLIC_SENDMAIL = async (req, res, next) => {
+  const {
+    receiver_email,
+    subject,
+    message,
+    sender_name,
+    sender_email,
+  } = req.body;
+
+  // make sure all required fields are provided
+  if (!receiver_email || !subject || !message) {
+    return res.status(400).json({
+      success: false,
+      message: `Please provide all required fields
+      receiver_email, subject, message.
+
+      HINT1: Make sure you are sending a POST request with the required fields in the body.
+      HINT2: Make sure field names are correct.
+      HINT3: sender_name and sender_email are optional
+      `,
+    });
+  }
+  try {
+    const data = {
+      email: receiver_email,
+      subject: subject,
+      sender_name: sender_name || "Bentility Public Mailer",
+      sender_email: sender_email || "NO-REPLY",
+      message: `
+        <p>${message}</p>
+
+        <p>Regards,</p>
+        <p>${sender_name}</p>
+        <p>${sender_email || ''}</p>
+
+
+        <small><i>This mailer is powered by <a href="https://bentility-api.herokuapp.com">Bentility API</a></i></small> <br />
+        <small>Made with ❤ by <i>
+        <a href="https://qbentil.com">Shadrack Bentil</a></i></small>
+      `,
+    };
+    Mail.publicMailer(data, (info) => {
+      res.status(200).json({
+        success: true,
+        message: "Email sent successfully",
+        mail_id: info.messageId,
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 // get users
 export const getUsers = async (req, res, next) => {
@@ -66,16 +105,24 @@ export const getUsers = async (req, res, next) => {
 
     // remove password, role, token, confirmed, createdAt, updatedAt, __v from users
     const usersWithoutSensitiveData = users.map((user) => {
-      const { password, role, token, confirmed, createdAt, updatedAt, __v, ...data } = user._doc;
+      const {
+        password,
+        role,
+        token,
+        confirmed,
+        createdAt,
+        updatedAt,
+        __v,
+        ...data
+      } = user._doc;
       return data;
-    })
+    });
     res.status(200).json({
       success: true,
       message: "Users fetched successfully",
-      data:usersWithoutSensitiveData
-    })
-  }catch (error) {
+      data: usersWithoutSensitiveData,
+    });
+  } catch (error) {
     next(error);
   }
-}
-
+};
