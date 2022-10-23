e4835295a7bb658c40319b787bc3d739590254af
qbentil <bentilshadrack72@gmail.com>
2022-10-23 10:59:31 +0000

    Remove routs from public domain

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { AUTHENTICATION, GETREFRESHTOKEN, LOGOUT } from "../controllers/auth.controllers.js";
+import { confirmTokenUser, resetTokenRequest } from "../controllers/user.controllers.js";
+import { verifyResetToken } from "../middlewares/Verifications.js";
 
 const router = Router()
 
 router.post("/", AUTHENTICATION)
 router.get("/", LOGOUT)
 router.get("/refresh", GETREFRESHTOKEN)
+// REQUEST PASSWORD RESET TOKEN
+router.post("/forgot-password", resetTokenRequest);
+
+// VERIFY RESET PASSWORD TOKEN
+router.get("/verify-token", verifyResetToken, confirmTokenUser);
 
 export default router;
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -7,11 +7,6 @@ import {
 } from "../controllers/home.controllers.js";
 
 import express from "express";
-import {
-  resetTokenRequest,
-  confirmTokenUser,
-} from "../controllers/user.controllers.js";
-import { verifyResetToken } from "../middlewares/Verifications.js";
 
 // init express router
 const router = express.Router();
@@ -23,12 +18,6 @@ router.get("/users", getUsers);
 
 // SEND EMAIL
 router.post("/email", SENDMAIL);
-
-// REQUEST PASSWORD RESET TOKEN
-router.post("/forgot-password", resetTokenRequest);
-
-// VERIFY RESET PASSWORD TOKEN
-router.get("/verify-token", verifyResetToken, confirmTokenUser);
 
 // Public Utils
 router.post("/public/sendmail", PUBLIC_SENDMAIL);
